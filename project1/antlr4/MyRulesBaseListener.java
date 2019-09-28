8c75b076c29b37c52b1c08239bf9692b71ef422a
peytoneli <45838686+peytoneli@users.noreply.github.com>
2019-09-27 22:07:19 -0500

    Insert Command (without expr parsing)
    Implemented Insert cmd, fixed Table constructor to initialize attributeTypes, fixed spacing in DBMS.java, removed prntln() commands

--- project1/antlr4/MyRulesBaseListener.java
+++ project1/antlr4/MyRulesBaseListener.java
@@ -11,13 +11,13 @@ public class MyRulesBaseListener extends RulesBaseListener {
     }
 
     @Override public void exitShowCmd(RulesParser.ShowCmdContext ctx) {
-        System.out.println("SHOW");
+        //System.out.println("SHOW");
     }
 
     @Override public void exitCreateCmd(RulesParser.CreateCmdContext ctx) {
         List<ParseTree> children = ctx.children;
         String tableName = children.get(2).getText();
-        System.out.println(tableName);
+
         LinkedHashMap<String,String> attributes = new LinkedHashMap<>(); //using linkedHashMap to guarantee insertion order
         ParseTree typedAttList = children.get(4);
         int typedAttCount = (typedAttList.getChildCount() + 1) / 3;
@@ -26,6 +26,7 @@ public class MyRulesBaseListener extends RulesBaseListener {
             String type = typedAttList.getChild(i*3 + 1).getText();
             attributes.put(attName,type);
         }
+
         ArrayList<String> primaryKeys = new ArrayList<>();
         ParseTree attList = children.get(9);
         int attCount = (attList.getChildCount() + 1) / 2;
@@ -35,6 +36,28 @@ public class MyRulesBaseListener extends RulesBaseListener {
         }
 
         myDBMS.createCmd(tableName, attributes, primaryKeys);
+    }
+
+    @Override public void exitInsertCmd(RulesParser.InsertCmdContext ctx) {
+        List<ParseTree> children = ctx.children;
+        String tableName = children.get(2).getText();
+        ArrayList<String> attributes = new ArrayList<>();
+
+        if(children.get(5).getText() == "RELATION"){
+            //TODO: parse expr here
+        } else {
+            //6 is the first index that a literal shows up
+            for(int i = 6 ; i < ctx.getChildCount() ; i += 2){
+                String str = children.get(i).getText();
+                // If the literal is a string, remove the quotations
+                if(str.charAt(0) == '"'){
+                    str = str.substring(1,str.length() - 1);
+                }
+                attributes.add(str);
+            }
+
+            myDBMS.insertCmd(tableName, attributes);
+        }
     }
 
 }
