ebd20a3c614b651f76972cba1d838d2e69ad8664
rconn478 <rconn478@gmail.com>
2019-09-29 17:05:11 -0500

    Completed return values of relational algebra expressions when parsing expressions

--- project1/antlr4/MyRulesBaseListener.java
+++ project1/antlr4/MyRulesBaseListener.java
@@ -167,10 +167,10 @@ public class MyRulesBaseListener extends RulesBaseListener {
                 Table renameTable = parseExpr(exprTree);
                 ArrayList<String> newAttNames = parseAttributeList(t.getChild(2));
                 return myDBMS.renameQry(renameTable, newAttNames);
-            case "Union" : break;
-            case "Difference" : break;
-            case "Product" : break;
-            case "NaturalJoin" : break;
+            case "Union" : return parseUnion(t.getChild(0));
+            case "Difference" : return parseDifference(t.getChild(0));
+            case "Product" : return parseProduct(t.getChild(0));
+            case "NaturalJoin" : return parseNaturalJoin(t.getChild(0));
         }
         return null;
     }
