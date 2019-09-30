be66ff7f1aa4834c343b56610523fdc072271b4b
rconn478 <rconn478@gmail.com>
2019-09-29 20:36:48 -0500

    Changed arraylist comparison to .equals()

--- project1/antlr4/MyRulesBaseListener.java
+++ project1/antlr4/MyRulesBaseListener.java
@@ -170,27 +170,19 @@ public class MyRulesBaseListener extends RulesBaseListener {
     }
 
     public Table parseUnion(ParseTree t) {
-        ParseTree exprA = t.getChild(0);
-        ParseTree exprB = t.getChild(2);
-        return myDBMS.unionQry(parseAtomicExpr(exprA), parseAtomicExpr(exprB));
+        return myDBMS.unionQry(parseExpr(t.getChild(0)), parseExpr(t.getChild(2)));
     }
 
     public Table parseDifference(ParseTree t) {
-        ParseTree exprA = t.getChild(0);
-        ParseTree exprB = t.getChild(2);
-        return myDBMS.differenceQry(parseAtomicExpr(exprA), parseAtomicExpr(exprB));
+        return myDBMS.differenceQry(parseExpr(t.getChild(0)), parseExpr(t.getChild(2)));
     }
 
     public Table parseProduct(ParseTree t) {
-        ParseTree exprA = t.getChild(0);
-        ParseTree exprB = t.getChild(2);
-        return myDBMS.productQry(parseAtomicExpr(exprA), parseAtomicExpr(exprB));
+        return myDBMS.productQry(parseExpr(t.getChild(0)), parseExpr(t.getChild(2)));
     }
 
     public Table parseNaturalJoin(ParseTree t) {
-        ParseTree exprA = t.getChild(0);
-        ParseTree exprB = t.getChild(2);
-        return myDBMS.naturalJoinQry(parseAtomicExpr(exprA), parseAtomicExpr(exprB));
+        return myDBMS.naturalJoinQry(parseExpr(t.getChild(0)), parseExpr(t.getChild(2)));
     }
 
     public Table parseExpr(ParseTree t){
