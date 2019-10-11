228ad557c8ef33bb7d184d6fa6e82b5e80de7535
rconn478 <rconn478@gmail.com>
2019-10-11 12:36:34 -0500

    Added example for query function

--- project1/Main.java
+++ project1/Main.java
@@ -8,38 +8,23 @@ import java.util.Scanner;
 
 import project1.antlr4.MyRulesBaseListener;
 import project1.antlr4.RulesLexer;
-import project1.antlr4.RulesParser;
-import project1.conditional.*;
 import org.antlr.v4.runtime.CharStream;
 import org.antlr.v4.runtime.CharStreams;
 import org.antlr.v4.runtime.CommonTokenStream;
 import org.antlr.v4.runtime.tree.ParseTreeWalker;
+import project1.antlr4.RulesParser;
 
 
 public class Main {
     public static void main(String[] args) throws FileNotFoundException {
-        //unitTesting();
-        File file = new File("project1/input.txt");
-        Scanner scanner = new Scanner(file);
-        List<String> lines = new ArrayList<>();
-        while (scanner.hasNextLine()) {
-            String line = scanner.nextLine();
-            if (line.length() != 0) { lines.add(line); }
-        }
-        MyRulesBaseListener listener = new MyRulesBaseListener();
-        for (String line : lines) {
-            CharStream charStream = CharStreams.fromString(line);
-            RulesLexer lexer = new RulesLexer(charStream);
-            CommonTokenStream commonTokenStream = new CommonTokenStream(lexer);
-            RulesParser parser = new RulesParser(commonTokenStream);
-            RulesParser.ProgramContext programContext = parser.program();
-            ParseTreeWalker walker = new ParseTreeWalker();
-            walker.walk(listener, programContext);
-        }
+        unitTesting();
+        //example query:
+        //System.out.println(listener.getMyDBMS().query(listener, "dogs + cats;").showTable());
     }
 
     public static void unitTesting() throws FileNotFoundException{
         int numOfTests = 3;
+        MyRulesBaseListener listener = new MyRulesBaseListener();
         for(int i = 1 ; i <= numOfTests ; i++) {
             File file = new File("tests/test" + String.valueOf(i) + ".txt");
             Scanner scanner = new Scanner(file);
@@ -50,7 +35,6 @@ public class Main {
                     lines.add(line);
                 }
             }
-            MyRulesBaseListener listener = new MyRulesBaseListener();
             for (String line : lines) {
                 CharStream charStream = CharStreams.fromString(line);
                 RulesLexer lexer = new RulesLexer(charStream);
